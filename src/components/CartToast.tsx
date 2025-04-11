420fe8b4b3999373769a976f30774ff8f72a5e11
Ahmed Fawzi <85083711+AhmedFawzyMof@users.noreply.github.com>
2025-04-11 19:45:53 +0200

    fixing cart toast

--- src/components/CartToast.tsx
+++ src/components/CartToast.tsx
@@ -1,17 +1,31 @@
 import { Button } from "flowbite-react";
 import { useCartStore } from "../store/CartStore";
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function CartToast() {
   const cartStore = useCartStore((state) => state);
+  const [showToast, setShowToast] = useState(false);
   const pathname = useLocation().pathname;
 
-  return pathname === "/cart" || pathname === "/order" ? null : (
+  useEffect(() => {
+    if (cartStore.getTotalQuantity() > 0) {
+      setShowToast(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (pathname === "/cart" || pathname === "/order") {
+      setShowToast(false);
+    }
+  }, [pathname]);
+
+  return showToast ? (
     <div className="fixed bottom-10 right-4 z-30 bg-white p-3 shadow-lg rounded flex flex-col gap-3">
       <p>عدد المنتجات في سلة التسوق: {cartStore.getTotalQuantity()}</p>
       <Button href="/cart" className="bg-primary font-bold">
         انتقل إلى عربة التسوق لإكمال الطلب
       </Button>
     </div>
-  );
+  ) : null;
 }
