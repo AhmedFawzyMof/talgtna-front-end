783e85e425dfd3f465e79d4eecc024c40ef9e34a
Ahmed Fawzi <85083711+AhmedFawzyMof@users.noreply.github.com>
2025-04-28 22:05:47 +0300

    new header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,25 @@
-import { Link } from "react-router-dom";
-import logoImg from "../assets/logo.png";
-import { useEffect, useState } from "react";
-import { useCartStore } from "../store/CartStore";
+import { Link, useNavigate } from "react-router-dom";
+import { IMAGE_BASE_URL } from "../config/config";
+import { MdMenuOpen } from "react-icons/md";
+import { BsBasket2Fill } from "react-icons/bs";
+import { FaHeart, FaDownload } from "react-icons/fa";
 import { useAuthStore } from "../store/AuthStore";
-import { IoIosCart, IoIosMenu, IoIosHeart, IoIosSearch } from "react-icons/io";
 import { Badge } from "flowbite-react";
-import { useNavigate } from "react-router-dom";
-import { FaDownload } from "react-icons/fa";
+import { useCartStore } from "../store/CartStore";
+import { BiSearch } from "react-icons/bi";
+import { useEffect, useState } from "react";
 
 interface BeforeInstallPromptEvent extends Event {
   prompt: () => Promise<void>;
   userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
 }
 
-function Navbar() {
-  const [isActive, setIsActive] = useState(false);
-  const [isSearchOpen, setSearchOpen] = useState(false);
+export default function Navbar() {
+  const authStore = useAuthStore((state) => state);
+  const cartStore = useCartStore((state) => state);
   const [query, setQuery] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const navigate = useNavigate();
   const [deferredPrompt, setDeferredPrompt] =
     useState<BeforeInstallPromptEvent | null>(null);
   const [isInstallable, setIsInstallable] = useState(false);
@@ -25,12 +28,10 @@ function Navbar() {
   );
   const isInStandaloneMode =
     "standalone" in window.navigator && (window.navigator as any).standalone;
-  const authStore = useAuthStore((state) => state);
-  const totalQuantity = useCartStore((state) => state.getTotalQuantity());
-  const navigate = useNavigate();
 
   useEffect(() => {
     const handler = (e: Event) => {
+      console.log("beforeinstallprompt event fired");
       e.preventDefault();
       setDeferredPrompt(e as BeforeInstallPromptEvent);
       setIsInstallable(true);
@@ -46,196 +47,192 @@ function Navbar() {
     }
   }, []);
 
-  const handleInstallClick = async () => {
-    if (!deferredPrompt) return;
-
-    deferredPrompt.prompt();
-    await deferredPrompt.userChoice;
-
-    setDeferredPrompt(null);
-    setIsInstallable(false);
+  const handleLogout = () => {
+    authStore.logout();
+    navigate("/login");
   };
 
   const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!query) return;
-    setSearchOpen(false);
     setQuery("");
     navigate(`/search?query=${query}`);
   };
 
-  const handelLogout = () => {
-    authStore.logout();
-    navigate("/");
+  const handleInstallClick = async () => {
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      console.log("User accepted the PWA installation");
+    } else {
+      console.log("User dismissed the PWA installation");
+    }
+    setDeferredPrompt(null); // Clear the prompt
+    setIsInstallable(false);
   };
 
   return (
-    <nav className="relative h-14 w-full bg-white shadow-sm flex items-center justify-between px-2 md:px-6 gap-5">
-      <div className="flex items-center gap-2 md:gap-5">
-        <button id="menuBtn" onClick={() => setIsActive(!isActive)}>
-          <IoIosMenu className="text-3xl" />
-        </button>
-        <Link
-          onClick={() => setIsActive(false)}
-          to="/cart"
-          className="cart relative"
-        >
-          {totalQuantity > 0 ? (
-            <Badge className="absolute -top-2 -right-2 bg-primary text-white font-bold">
-              {totalQuantity}
-            </Badge>
-          ) : null}
-          <IoIosCart className="text-3xl" />
-          <p className="text-3xl sr-only">سلة الشراء</p>
-        </Link>
-        {authStore.isAuthenticated ? (
-          <Link
-            onClick={() => setIsActive(false)}
-            to="/favorites"
-            className="relative"
-          >
-            <IoIosHeart className="text-3xl" />
-            {authStore.favorites > 0 ? (
-              <Badge className="absolute -top-2 -right-2 bg-primary text-white font-bold">
-                {authStore.favorites}
-              </Badge>
-            ) : null}
-            <p className="text-3xl sr-only">المفضلة</p>
-          </Link>
-        ) : null}
-        {isInstallable && (
-          <button onClick={handleInstallClick}>
-            <FaDownload className="text-3xl" />
-          </button>
-        )}
-        <button
-          id="searchBtn"
-          className="md:hidden"
-          onClick={() => setSearchOpen(!isSearchOpen)}
-        >
-          <IoIosSearch className="text-3xl" />
-        </button>
-        <form
-          action="/search"
-          className="hidden md:block relative w-[60%]"
-          onSubmit={(e) => handelSubmit(e)}
-        >
-          <input
-            type="search"
-            name="query"
-            placeholder="ابحث عن منتجاتك ..."
-            className="w-full h-full border border-primary rounded-md px-3 py-1"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
+    <header dir="ltr" className="sticky top-0 z-50 bg-white shadow ">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+        <Link to="/" className="block text-teal-600">
+          <span className="sr-only">الرئيسية</span>
+          <img
+            src={IMAGE_BASE_URL + "/img/logo.png"}
+            alt="Talgtna"
+            className="h-10 w-auto"
           />
-          <button
-            type="submit"
-            className="absolute bg-primary top-1/2 -translate-y-1/2 w-11 left-0 h-full rounded-l-md flex items-center justify-center"
-          >
-            <IoIosSearch className="text-3xl text-white text-center" />
-          </button>
-        </form>
-      </div>
-      <Link onClick={() => setIsActive(false)} to="/" className="logo h-full">
-        <img src={logoImg} alt="logo" className=" w-30 h-full" />
-      </Link>
+        </Link>
 
-      <div
-        className={`menu absolute bg-white h-[90vh] w-[250px] top-[105%] rounded shadow-xl z-50 right-0 flex flex-col gap-4 p-4 duration-300 transition-all ${
-          isActive ? "translate-x-0" : " translate-x-[250px]"
-        }`}
-      >
-        <Link
-          onClick={() => setIsActive(false)}
-          to="/"
-          className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
+        <div
+          dir="rtl"
+          className="ml-auto flex items-center justify-start md:justify-between gap-2"
         >
-          الصفحة الرئيسية
-        </Link>
-        {isInstallable && (
           <button
-            id="searchBtn"
-            className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded flex gap-2 items-center "
-            onClick={handleInstallClick}
+            onClick={() => setIsActive(!isActive)}
+            className="bg-primary text-white py-2 px-2 rounded shadow"
           >
-            تثبيت التطبيق <FaDownload className="text-xl" />
+            <span className="sr-only">القائمة</span>
+            <MdMenuOpen className="text-xl" />
           </button>
-        )}
-        {isIos && isInstallable && (
-          <div className="p-3 bg-yellow-200 text-sm rounded shadow">
-            لتثبيت التطبيق، اضغط على <strong>مشاركة</strong> ثم اختر{" "}
-            <strong>"إضافة إلى الشاشة الرئيسية"</strong>.
-          </div>
-        )}
-        {authStore.isAuthenticated ? (
           <Link
-            onClick={() => setIsActive(false)}
-            to="/order/history"
-            className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
+            to="/cart"
+            className="relative bg-primary text-white py-2 px-2 rounded shadow"
           >
-            سجل الطلبات
+            <span className="sr-only">سلة التسوق</span>
+            {cartStore.getTotalQuantity() > 0 && (
+              <Badge className="absolute -top-2 -left-3 z-50" color="success">
+                {cartStore.getTotalQuantity()}
+              </Badge>
+            )}
+            <BsBasket2Fill className="text-xl" />
           </Link>
-        ) : null}
-        <Link
-          onClick={() => setIsActive(false)}
-          to="/contact"
-          className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
-        >
-          تواصل معنا
-        </Link>
-        <Link
-          onClick={() => setIsActive(false)}
-          to="/about"
-          className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
-        >
-          معلومات عنا
-        </Link>
-        <Link
-          onClick={() => setIsActive(false)}
-          to="/complaint"
-          className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
-        >
-          الشكوي
-        </Link>
-        {authStore.isAuthenticated && (
-          <button
-            onClick={handelLogout}
-            className="w-full py-2 duration-300 hover:bg-primary hover:pr-1 hover:text-white rounded"
-          >
-            تسجيل الخروج
-          </button>
-        )}
+          {authStore.isAuthenticated && (
+            <Link
+              to="/favorites"
+              className="relative bg-primary text-white py-2 px-2 rounded shadow"
+            >
+              <span className="sr-only">المفضلة</span>
+              {authStore.favorites > 0 && (
+                <Badge className="absolute z-50 -top-2 -left-3" color="success">
+                  {authStore.favorites}
+                </Badge>
+              )}
+              <FaHeart className="text-xl" />
+            </Link>
+          )}
+          {isInstallable && (
+            <button
+              onClick={handleInstallClick}
+              className="relative bg-primary text-white py-2 px-2 rounded shadow"
+            >
+              <FaDownload className="text-xl" />
+            </button>
+          )}
+        </div>
       </div>
       <div
-        className={`h-14 bg-white duration-300 w-full absolute ${
-          isSearchOpen ? "z-10" : "-z-10"
-        } left-0 top-[56px] shadow-md grid place-items-center ${
-          isSearchOpen ? "translate-y-0" : "translate-y-[-100%]"
-        }`}
+        dir="rtl"
+        className={`absolute right-0 w-64 ${
+          isActive ? "translate-x-0" : "translate-x-full"
+        } duration-300 ease-in-out flex h-screen flex-col justify-between border-e border-gray-100 bg-white`}
       >
-        <form
-          action="/search"
-          className="relative w-full"
-          onSubmit={(e) => handelSubmit(e)}
-        >
-          <input
-            type="search"
-            name="query"
-            placeholder="ابحث عن منتجاتك ..."
-            className="w-full h-full border border-primary rounded-md px-3 py-1"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="absolute bg-primary top-1/2 -translate-y-1/2 w-11 left-0 h-full rounded-l-md flex items-center justify-center"
-          >
-            <IoIosSearch className="text-3xl text-white text-center" />
-          </button>
-        </form>
+        <div className="px-4 py-6">
+          <ul className="mt-6 space-y-3">
+            <li>
+              <form onSubmit={handelSubmit} className="relative">
+                <input
+                  type="search"
+                  name="search"
+                  className="w-full rounded-md border-2 border-primary-200 py-2 pr-10 focus:border-primary-200"
+                  placeholder="بحث ..."
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  autoComplete="off"
+                />
+                <button
+                  type="submit"
+                  className="absolute top-0 right-0 bg-primary text-white w-10 h-full grid place-items-center rounded-r-md"
+                >
+                  <BiSearch className="text-2xl" />
+                </button>
+              </form>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+              >
+                الصفحة الرئيسية
+              </Link>
+            </li>
+            {isInstallable && (
+              <li>
+                <button
+                  onClick={handleInstallClick}
+                  className="flex items-center gap-2 w-full text-start rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+                >
+                  <p>تحميل التطبيق</p>
+                  <FaDownload />
+                </button>
+              </li>
+            )}
+            <li>
+              <Link
+                to="/about"
+                className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+              >
+                من نحن
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+              >
+                تواصل معنا
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/complaint"
+                className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+              >
+                شكوى
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/policy"
+                className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+              >
+                سياسة الخصوصية
+              </Link>
+            </li>
+            {authStore.isAuthenticated && (
+              <>
+                <li>
+                  <Link
+                    to="/order/history"
+                    className="block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+                  >
+                    سجل الطلبات
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    onClick={handleLogout}
+                    className="w-full text-start block rounded-lg bg-primary-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-primary-400 hover:text-white duration-300 ease-in-out"
+                  >
+                    تسجيل الخروج
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
-    </nav>
+    </header>
   );
 }
-
-export default Navbar;
